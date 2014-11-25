831418281dd64538e6f1eca058f4251eab760139
Kevin Daniels <kevindan999@gmail.com>
2014-11-25 17:15:14 -0500

    added start button

--- OddColorOut/src/com/example/oddcolorout/MainActivity.java
+++ OddColorOut/src/com/example/oddcolorout/MainActivity.java
@@ -4,9 +4,12 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity
+{
+	Button start;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
